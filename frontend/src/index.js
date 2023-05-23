ec015bd3f8d36ffc12e8dbf2994efedb3d4425f9
LNevaLie <namly1908@gmail.com>
2023-05-23 15:13:11 +0700

    reformat routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 
 import './index.css';
 import App from './pages/App/App';
@@ -11,14 +9,7 @@ import ForgetPassword from './pages/Forget Password/Forget Password';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/signUp' element={<SignUp />} />
-                <Route path='/signIn' element={<SignIn />} />
-                <Route path='/forgetPassword' element={<ForgetPassword />} />
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 )
--- frontend/src/pages/App/App.js
+++ frontend/src/pages/App/App.js
@@ -2,15 +2,23 @@ import React from "react";
 
 import { Header, Intro, AppointmentView } from "../../containers";
 // import { Navbar } from "../../components";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import "./App.css";
+import SignIn from "../Sign In/Sign In";
+import SignUp from "../Sign Up/Sign Up";
+import ForgetPassword from "../Forget Password/Forget Password";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Header />  */}
-      {/* <Intro />  */}
-      <AppointmentView /> 
+     <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<SignIn />} />
+                <Route path='/signUp' element={<SignUp />} />
+                <Route path='/forgetPassword' element={<ForgetPassword />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 };
