1e6f925e0302cd6c36a147c76bd02e82fc983300
minhth1412 <76166997+minhth1412@users.noreply.github.com>
2023-06-03 20:07:53 +0700

    Add onClick for userEdit, route to server.js

--- frontend/src/pages/user/UserEdit.jsx
+++ frontend/src/pages/user/UserEdit.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "../page.css";
 import style from "../../components/button/button.module.css";
 
 const UserEdit = () => {
   // const pageTitle = "Chỉnh sửa thông tin cá nhân";
+
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [addr, setAddr] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (name === "" && dob === "" && phone === "" && email === "" && addr === "") {
+      alert("Vui lòng không để trống")
+    }
+    else {
+      axios
+        .post("http://localhost:8800/useredit", { name, dob, phone, email, addr })
+        .then((res) => {
+          if (res.data === "success") {
+            alert('Sửa đổi thông tin thành công!');
+          }
+          // else if (res.data === "empty username"){
+          //   alert('Tên đăng nhập không được để trống!');
+          // }
+          // else if (res.data === "empty password"){
+          //   alert('Mật khẩu không được để trống!');
+          // }
+
+          else alert(res.data);;;
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
   return (
     <>
       <div className="page-container">
@@ -18,12 +51,13 @@ const UserEdit = () => {
                 id="full-name"
                 name="full-name"
                 placeholder="Nguyễn Văn A"
+                onChange={(e) => setName(e.target.value)}
               />
             </label>
 
             <label for="DoB">
               <p>Ngày sinh</p>
-              <input type="date" id="DoB" name="DoB" placeholder="01/01/2002" />
+              <input type="date" id="DoB" name="DoB" placeholder="01/01/2002" onChange={(e) => setDob(e.target.value)}/>
             </label>
 
             <label for="telephone">
@@ -33,6 +67,7 @@ const UserEdit = () => {
                 id="telephone"
                 name="telephone"
                 placeholder="0123456789"
+                onChange={(e) => setPhone(e.target.value)}
               />
             </label>
 
@@ -43,20 +78,23 @@ const UserEdit = () => {
                 id="email"
                 name="email"
                 placeholder="username@gmail.com"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </label>
 
             <label for="address">
               <p>Địa chỉ</p>
-              <textarea type="text" id="address" name="address" rows={1} />
+              <textarea type="text" id="address" name="address" rows={1} onChange={(e) => setAddr(e.target.value)}/>
             </label>
           </form>
         </div>
 
         <div className="page_action">
           <Link to="/userEdit">
-            <button className={`${style.button} ${style.green}`}>
-              Xác nhận
+            <button 
+              onClick={handleSubmit}
+              className={`${style.button} ${style.green}`}
+            > Xác nhận
             </button>
           </Link>
         </div>
