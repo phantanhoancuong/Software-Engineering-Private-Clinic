836ace4462f4544b2c8109d445b950b2cbfbc56f
minhth1412 <76166997+minhth1412@users.noreply.github.com>
2023-06-03 20:06:55 +0700

    Add randomID for user when signup successfully

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const mysql = require('mysql')
 const cors = require('cors')
 
@@ -27,7 +27,7 @@ app.post("/signin", (req, res) => {
         req.body.email,
         req.body.password
     ]
-    if (!req.body.name.trim())
+    if (!req.body.email.trim())
         return res.json('Tên đăng nhập trống!');
     if (!req.body.password.trim())
         return res.json('Mật khẩu trống!');
@@ -38,12 +38,39 @@ app.post("/signin", (req, res) => {
     })
 })
 
+function generate(n) {
+    var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
+    if ( n > max ) { return generate(max) + generate(n - max); }
+    
+    max = Math.pow(10, n+add);
+    var min = max/10; // Math.pow(10, n) basically
+    var number = Math.floor( Math.random() * (max - min + 1) ) + min;
+    
+    return (" BN_" + number).substring(add); 
+}
+
 app.post("/signup", (req, res) => {
-    const sql = "INSERT INTO `user` (`name`, `email`, `password`) VALUES (?)";
+    const sql = "INSERT INTO `user` (`name`, `email`, `password`, `ID`) VALUES (?)";
+    var ID = `${generate(5)}`;
+    const checkID = "SELECT * FROM `user` WHERE `ID` = ?";
+    var check = true;
+    var tempData = '';
+    
+    // get compare ID later, below compare ID is incomplete!
+    while (db.query(checkID, [ID], (err, data) => {tempData == data;})) {
+        if (tempData === ""){
+            check = false;
+        };
+        if (check === false)
+            break;
+        ID = `${generate(5)}`;
+        console.log(`: ${tempData}`);
+    };
     const values = [
         req.body.name,
         req.body.email,
-        req.body.password
+        req.body.password,
+        ID
     ];
 
     console.log(values);
@@ -60,6 +87,35 @@ app.post("/signup", (req, res) => {
     db.query(sql, [values], (err, data) => {
         if (err) {
             return res.json('Email này đã được đăng ký!');
+        }
+        return res.json(ID);
+    })
+})
+
+app.post("/useredit", (req, res) => {
+    
+    // sql command below is unfinished, req need an identity user who logged in (ID) and need to add pk
+    // below need to fix from INSERT INTO to UPDATE
+    const sql = "INSERT INTO `user` (`name`, `email`, `dob`, `phone`, `address`) VALUES (?)";
+    const data = [
+        req.body.name,
+        req.body.email,
+        req.body.dob,
+        req.body.phone,
+        req.body.addr,
+    ];
+
+    console.log(data);
+    //if (!req.body.name.trim())
+    //    return res.json('Tên đăng nhập trống!');
+    //if (!req.body.password.trim())
+    //    return res.json('Mật khẩu trống!');
+    //if (!req.body.email.trim())
+    //    return res.json('Email đăng ký trống!');
+
+    db.query(sql, [data], (err, data) => {
+        if (err) {
+            return res.json('Unfinished, fix later in server.js!');
         }
         return res.json('success');
     })
