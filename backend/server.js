1ab1fc938e9a26d6b22d259eae20aff4354a627f
LNevaLie <namly1908@gmail.com>
2023-06-14 18:00:30 +0700

    fix medical report create condition

--- backend/server.js
+++ backend/server.js
@@ -329,7 +329,7 @@ app.post("/appointmentviewModal", (req, res) => {
 app.post("/medicalreportcreate", (req, res) => {
     const sql = "INSERT INTO `medicalbill` (`date`, `ID`, `symptom`, `diagnose`) VALUES (?)";
     const checkID = "SELECT ID FROM `patient` WHERE `ID` = ?";
-    const checkDup = "SELECT ID FROM `medicalbill` WHERE `ID` = ? AND `date` = ?";
+    const checkDup = "SELECT * FROM `medicalbill` WHERE `ID` = ? AND `date` = ?";
 
     const values = [
         req.body.date,
@@ -347,12 +347,12 @@ app.post("/medicalreportcreate", (req, res) => {
             return res.json("ID bệnh nhân không đúng!")
         }
         else {
-            db.query(checkDup, [req.body.date, req.body.date], (err, data) => {
+            db.query(checkDup, [req.body.id, req.body.date], (err, data) => {
                 if(err) {
                     console.log(err)
                     return res.json("Lỗi 2")
                 }
-                else if (data.length >= 0) {
+                else if (data.length > 0) {
                     return res.json("Phiếu khám đã tồn tại")
                 }
                 else {
